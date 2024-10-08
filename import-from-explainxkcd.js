18f3c67446a3903fbf9dd60617fde8d4c5af0944
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-10-08 15:09:20 +0200

    Add a node.js script to (re-)import content from explainXKCD
    The wonderful site at https://www.explainxkcd.com/ was clearly the
    source of the content stored in this here repository.
    
    But it has not been synchronized in quite a while, which is too bad
    because XKCD is this wonderful source of joy and being able to quickly
    search it is a very, very nice thing.
    
    So let's add a somewhat simplistic node.js script that takes on its
    standard input an XML dump of the most recent revisions of the pages on
    that site.
    
    The idea is to remove the files in `content/comics/` and then run this
    script to re-generate them.
    
    Note that this script does not by any form or shape claim to be a
    complete converter from Wikitext to Markdown! It is _just_ good enough
    for the task at hand, and if gaps are found in the conversion, the idea
    is to adapt the script accordingly and re-import the pages.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ import-from-explainxkcd.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+
+/*
+ * The idea is to feed this script on `stdin` an XML dump obtained from
+ * https://www.explainxkcd.com/wiki/index.php/Special:Export, using "All_comics"
+ * as category, clicking "Add", then "Export".
+ */
+
+(async () => {
+  const entities = {
+    'nbsp': ' ',
+    'amp' : '&',
+    'quot': '"',
+    'lt'  : '<',
+    'gt'  : '>',
+    'eacute' : 'é',
+    'acirc'  : 'å',
+    'euro'   : '€',
+    'oelig'  : 'œ',
+    'rdquo'  : '”',
+    'trade'  : '™',
+  };
+  const entitiesRegexp = new RegExp(`&(${Object.keys(entities).join('|')});`, 'g')
+  const decodeEntities = encoded =>
+      encoded.replace(entitiesRegexp, (_, entity) => entities[entity])
+        .replace(/&#(\d+);/g, (_, num) => String.fromCharCode(parseInt(num, 10)))
+
+  const leftPad = (num, pad) => pad ? new String(num).padStart(2, '0') : num
+  const dateStringToKey = (date, pad) => {
+    const d = new Date(date)
+if (`${d.getFullYear()}` == 'NaN') throw new Error(`NaN for date ${date}, ${d}`)
+    return `${d.getFullYear()}-${leftPad(1 + d.getMonth(), pad)}-${leftPad(d.getDate(), pad)}`
+  }
+
+  const fs = require('fs')
+  const readline = require('readline')
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  })
+
+  let page = {}
+  const chunks = []
+  let text = false
+
+  for await (const line of rl) {
+    // Handle <text>: Its content is multi-line
+    if (text) {
+      const match = line.match(/^(.*)<\/text> *$/)
+      if (!match) chunks.push(line)
+      else {
+        chunks.push(match[1])
+        page.text =
+          chunks
+            .join('\n')
+            .replace(/[]/g, '')
+        chunks.splice(0)
+        text = false
+      }
+      continue
+    } else {
+      const match = line.match(/^ *<text[^>]*>(.*)$/)
+      if (match) {
+        const match2 = match[1].match(/(.*)<\/text>/)
+        if (match2) chunks.push(match2[1])
+        else {
+          chunks.push(match[1])
+          text = true
+        }
+        continue
+      }
+    }
+
+    // Handle all other tags
+    const match = line.match(/^ *<(\/?)([^ >]+)([^>]*)> *((.*)<\/[^>]*>)? *$/)
+    if (!match) continue
+    const [, end, tag, attrs, c2, content] = match
+
+    if (tag === 'page') {
+      if (!end) page = {}
+      // This is a duplicate of '590: Papyrus' and actually only contains `{{:590: Papyrus}}`
+      else if (page.title === '590: Papyrus/Font') ; // do nothing
+      else {
+
+        page.infobox =
+          (page.text.match(/(?:^|\n){{ *comic *\n([^]*?)(\n *)?}}/i)?.[1] || '')
+            .split('\n').reduce((o, v) => {
+              const match = v.match(/^ *\| *(\S+) *= *(.*)/)
+              if (match) o[match[1]] = match[2].trim()
+              return o
+            }, {})
+
+        // Fix unparseable dates
+        page.infobox.date =
+          (page.infobox.date || '')
+            .replace(/(\d+)(st|nd|rd|th), */, '$1 ')
+            .replace(/.*DO NOT ADD (2006-01-01).*/, '$1')
+            .replace(/ *&lt;.*/, '')
+
+        // Strip number prefix from title
+        page.title = page.title.replace(new RegExp(`^${page.infobox.number}:? *`), '')
+
+        // Add missing image
+        if (!page.infobox.image) page.infobox.image = {
+          '1331': 'frequency/heartbeat.gif',
+          '2250': 'ok_okay_ok_2x.png',
+        }[page.infobox.number]
+
+        page.transcript =
+          decodeEntities(
+            (page.text.match(/== *Transcript *==\n+([^]*?)\n(?:==|{{ *comic +discussion)/i)?.[1] || '')
+              .split('\n')
+              .filter(line => line !== '')
+              .map(line => line.replace(/^:+/, '').replace(/ +$/, ''))
+              .join('\n\n')
+              .replace(/[*_]/g, '\\$&')
+              .replace(/https?:\/\/[^ ]*\\_[^ ]*/g, e => e.replace(/\\_/g, '_'))
+              .replace(/'''/g, '**')
+              .replace(/''/g, '*')
+              .replace(/&lt;\/?nowiki&gt;/g, '')
+              .replace(/http:\/\//g, 'https://')
+              .replace(/\[(https:\/\/[^ ]+) ([^\]]+)\]/g, '[$2]($1)')
+          )
+
+        page.filePath = [
+          `content/comics/`,
+          dateStringToKey(page.infobox.date, false),
+          `-`,
+          page.title.toLowerCase().replace(/[^a-z0-9]/g, '-').replace(/--+/g, '-'),
+          `.md`,
+        ].join('')
+        page.fileContent = [
+          `---`,
+          `date: ${dateStringToKey(page.infobox.date, true)}`,
+          `title: "${page.title.replace(/[\\"]/g, '\\$&')}"`,
+          `num: ${page.infobox.number}`,
+          `alt: >-`,
+          `${decodeEntities(page.infobox.titletext || '')
+            .replace(/<br>.*/, '')
+            .split('\n')
+            .map(line => `  ${line}`) // indent
+            .join('\n')}`,
+          page.infobox.number === '404'
+          ? ''
+          : `img: https://imgs.xkcd.com/comics/${page.infobox.image.replace(/ /g, '_')}`,
+          `---`,
+          `${page.transcript}`
+        ].join('\n')
+
+        fs.writeFileSync(page.filePath, page.fileContent)
+      }
+    } else if (!end && tag === 'title') page.title = content
+  }
+})().catch(e => {
+  process.stderr.write(`${e.stack}\n`)
+  process.exit(1)
+})
